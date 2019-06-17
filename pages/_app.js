acd51ed28f667c46d0f5858f10431b87604abd31
Jack Doyle <jackdo@hotmail.co.uk>
2019-06-17 14:46:07 +0100

    Add comment to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,13 @@ const next = require('next');
 
 // Extending App provides MyApp with next.js functionality
 class MyApp extends App {
+  // getInitialProps is a next.js lifecycle method that runs before the first render
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
+    // If component has props
     if (Component.getInitialProps) {
+      // getInitialProps will crawl the page for any queries or mutations that need to be fetched.
+      // They need to be fired off and resolved before the page can be rendered.
       pageProps = await Component.getInitialProps(ctx);
     }
     // this exposes the query to the user
@@ -20,6 +24,7 @@ class MyApp extends App {
 
     return (
       <Container>
+        {/* This will expose the Apollo Client to the application */}
         <ApolloProvider client={apollo}>
           <Page>
             {/* Component will be whichever page is visited eg. Sell.js */}
@@ -30,5 +35,5 @@ class MyApp extends App {
     );
   }
 }
-
+// withData is a higher order component. This will make the apollo client accessible via props.
 export default withData(MyApp);
