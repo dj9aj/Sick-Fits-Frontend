5ccec4773f0110d4848dc39ec5820819e474d37d
Jack Doyle <jackdo@hotmail.co.uk>
2019-06-20 16:31:06 +0100

    Add user component render prop

--- components/Nav.js
+++ components/Nav.js
@@ -3,6 +3,14 @@ import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {/* The data returned is a result of the CURRENT_USER_QUERY */}
+      {({ data: { me } }) => {
+        console.log(me);
+        if (me) return <p>{me.name}</p>;
+        return null;
+      }}
+    </User>
     {/* Rather than attach a href to the anchor tag, wrap the anchor tag in a Link tag. This will bind the link to HTML5 push state. */}
     <Link href="/items">
       <a>Shop</a>
